02ce40be5d1cb0226f6ce5b34901393c351d1fd1
papi <papi@papi-ux.com>
2026-08-19 14:50:08 -0400

    feat(web): refresh resources and add sponsor link (#498)

--- src_assets/common/assets/web/resource-links.js
+++ src_assets/common/assets/web/resource-links.js
@@ -11,12 +11,24 @@
  */
 
 export const resources = [
+  { href: 'https://papi-ux.com/', labelKey: 'resource_card.website' },
   { href: 'https://papi-ux.com/docs/', labelKey: 'resource_card.documentation' },
   { href: 'https://papi-ux.com/docs/troubleshooting/', labelKey: 'resource_card.troubleshooting_guide' },
+  { href: 'https://github.com/papi-ux/polaris', labelKey: 'resource_card.github' },
+  { href: 'https://github.com/papi-ux/polaris/releases', labelKey: 'resource_card.releases' },
   { href: 'https://github.com/papi-ux/polaris/discussions', labelKey: 'resource_card.github_discussions' },
   { href: 'https://github.com/papi-ux/polaris/wiki', labelKey: 'resource_card.github_wiki' },
   { href: 'https://github.com/papi-ux/polaris/wiki/Stuttering-Clinic', labelKey: 'resource_card.github_stuttering_clinic' }
 ]
+
+// Sponsorship is deliberately not a primary resource button. It belongs in the
+// System resource area, but should remain a quiet invitation rather than a
+// prerequisite for finding support or documentation.
+export const sponsor = {
+  href: 'https://github.com/sponsors/papi-ux',
+  labelKey: 'resource_card.sponsor',
+  ariaLabelKey: 'resource_card.sponsor_desc'
+}
 
 export const legalDocs = [
   { href: 'https://github.com/papi-ux/polaris/blob/master/LICENSE', labelKey: 'resource_card.license' },
